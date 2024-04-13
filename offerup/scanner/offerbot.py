329daf0ef0a2fb3b8593a68794933691bca3a729
Hunter Boyd <hunterjsb@gmail.com>
2024-04-13 11:15:49 -0400

    sends message

--- offerup/scanner/offerbot.py
+++ offerup/scanner/offerbot.py
@@ -4,10 +4,15 @@ from selenium import webdriver
 from pyOfferUp import fetch
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from offerup.config import Config
 
 ASK_XPATH = '/html/body/div[1]/div[5]/div[2]/main/div[1]/div/div[1]/div/div[3]/div[2]/div[2]'
+CHAT_XPATH = '/html/body/div[3]/div[3]/div/div[3]/form/div/div/div[2]/div'
+NEW_MSG_XPATH = '/html/body/div[3]/div[3]/div/div[3]/form/div/div/div[2]/div/textarea'
+SEND_MSG_XPATH = '/html/body/div[3]/div[3]/div/div[3]/form/button'
 
 
 class OfferBot:
@@ -27,8 +32,20 @@ class OfferBot:
             for listing in listings[model]:
                 url = listing["listingUrl"]
                 self.driver.get(url)
-                button = self.driver.find_element(By.XPATH, ASK_XPATH)
-                button.click()
+
+                ask_btn = self.driver.find_element(By.XPATH, ASK_XPATH)
+                ask_btn.click()
+
+                chat_btn = WebDriverWait(self.driver, 5).until(
+                    EC.presence_of_element_located((By.XPATH, CHAT_XPATH))
+                )
+                chat_btn.click()
+
+                msg_txt = WebDriverWait(self.driver, 5).until(
+                    EC.presence_of_element_located((By.XPATH, NEW_MSG_XPATH))
+                )
+                msg_txt.send_keys('Hello! Is this still available?')
+                self.driver.find_element(By.XPATH, SEND_MSG_XPATH).click()
                 time.sleep(1000)
 
     def get_listings(self) -> dict[str, list[dict]]:
