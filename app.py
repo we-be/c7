0262d267feda07a694274ccb89c12cec27dad144
EDaley-FlemCollege <eric.daley@flemingcollege.ca>
2024-05-06 19:32:24 -0400

    Updated App.py
    Added button for submission, expanders, started working on session states to allow for radio buttons to auto close expanders

--- app.py
+++ app.py
@@ -23,17 +23,26 @@ if st.checkbox('`C3: conversations/offerup`'):
     st.subheader('Raw data')
     st.write(data)
 
-
-def write_listing(_listing: dict):
-    st.subheader(f'{listing["originalTitle"]}: ${listing["originalPrice"]}')
-    st.write(listing["description"])
-    st.radio("Grade", Grade._member_names_, horizontal=True, key=_id)
-
+def write_listing(expander, radio_key, _id, _listing: dict):
+    expander.subheader(f'{listing["originalTitle"]}: ${listing["originalPrice"]}')
+    expander.write(listing["description"])
+    selected_grade = expander.radio("Grade", Grade._member_names_, horizontal=True, key=radio_key, index=None)
+    print(selected_grade)
     photos = listing["photos"]
-    st.image([photo["detail"]["url"] for photo in photos])
+    expander.image([photo["detail"]["url"] for photo in photos])
+    return selected_grade
 
+selected_grades = {}
 
 for _id in data.id:
     listing = fetch.get_listing_details(_id)["data"]["listing"]
-    st.sidebar.selectbox = listing["originalTitle"]
-    write_listing(listing)
+    radio_key = f"grade_{_id}"
+    if radio_key not in st.session_state:
+        st.session_state[radio_key] = True
+    expander = st.expander(f"Listing {listing['originalTitle']}  |  ID: {_id}", expanded=st.session_state[radio_key])
+    selected_grades[_id] = write_listing(expander, radio_key, _id, listing)
+
+if st.button("Print Selected Grades", key="submit_button"):
+    for _id, grade in selected_grades.items():
+        if grade is not None:
+            print(f"Grade selected for Listing ID {_id}: {grade}")
