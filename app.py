0aac1a0b8dda9a611d5d01f44bfe033c00218c76
EDaley-FlemCollege <eric.daley@flemingcollege.ca>
2024-05-06 20:00:16 -0400

    Removed session states

--- app.py
+++ app.py
@@ -23,24 +23,24 @@ if st.checkbox('`C3: conversations/offerup`'):
     st.subheader('Raw data')
     st.write(data)
 
-def write_listing(expander, radio_key, _id, _listing: dict):
-    expander.subheader(f'{listing["originalTitle"]}: ${listing["originalPrice"]}')
-    expander.write(listing["description"])
-    selected_grade = expander.radio("Grade", Grade._member_names_, horizontal=True, key=radio_key, index=None)
-    print(selected_grade)
+
+def write_listing(_listing: dict):
+    st.subheader(f'{listing["originalTitle"]}: ${listing["originalPrice"]}')
+    st.write(listing["description"])
+    selected_grade = st.radio("Grade", Grade._member_names_, horizontal=True, key=f"grade_{_id}", index=None)
+    if selected_grade is not None:
+        print(f"Grade selected for Listing ID {_id}: {selected_grade}")
+    
     photos = listing["photos"]
-    expander.image([photo["detail"]["url"] for photo in photos])
-    return selected_grade
+    st.image([photo["detail"]["url"] for photo in photos])
 
 selected_grades = {}
 
 for _id in data.id:
     listing = fetch.get_listing_details(_id)["data"]["listing"]
-    radio_key = f"grade_{_id}"
-    if radio_key not in st.session_state:
-        st.session_state[radio_key] = True
-    expander = st.expander(f"Listing {listing['originalTitle']}  |  ID: {_id}", expanded=st.session_state[radio_key])
-    selected_grades[_id] = write_listing(expander, radio_key, _id, listing)
+    # expanded = True if data.graded == None else False
+    with st.expander(f"Listing {listing['originalTitle']}  |  ID: {_id}", expanded=True):
+        selected_grades[_id] = write_listing(listing)
 
 if st.button("Print Selected Grades", key="submit_button"):
     for _id, grade in selected_grades.items():
