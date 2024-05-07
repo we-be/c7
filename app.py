3a0d390f44dc0a6b5f85ad4bf7bf35e956cf0026
EDaley-FlemCollege <eric.daley@flemingcollege.ca>
2024-05-06 20:33:15 -0400

    Added checkboxes to app.py
    No functionality has been added to Next button yet

--- app.py
+++ app.py
@@ -23,24 +23,36 @@ if st.checkbox('`C3: conversations/offerup`'):
     st.subheader('Raw data')
     st.write(data)
 
+NUM_CONTAINERS = len(data)
 
-def write_listing(_listing: dict):
+if 'expand' not in st.session_state:
+    st.session_state.expand = [True]*NUM_CONTAINERS
+
+def write_listing(i, _listing: dict):
     st.subheader(f'{listing["originalTitle"]}: ${listing["originalPrice"]}')
     st.write(listing["description"])
-    selected_grade = st.radio("Grade", Grade._member_names_, horizontal=True, key=f"grade_{_id}", index=None)
-    if selected_grade is not None:
-        print(f"Grade selected for Listing ID {_id}: {selected_grade}")
-    
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        selected_grade = st.radio("Grade", ['B','C','D'], horizontal=True, key=f"grade_{_id}", index=None)
+    with col2:
+        st.checkbox("Cracked Back", key=f"Back_{i}")
+        st.checkbox("Cracked Camera", key=f"Cam_{i}")
+    with col3:
+        st.checkbox("LCD Damage", key=f"LCD_{i}")
+        if st.button("Next", key=f"next_{i}"):
+            pass
     photos = listing["photos"]
     st.image([photo["detail"]["url"] for photo in photos])
 
+    return selected_grade
+
 selected_grades = {}
 
-for _id in data.id:
+for i, _id in enumerate(data.id):
     listing = fetch.get_listing_details(_id)["data"]["listing"]
     # expanded = True if data.graded == None else False
     with st.expander(f"Listing {listing['originalTitle']}  |  ID: {_id}", expanded=True):
-        selected_grades[_id] = write_listing(listing)
+        selected_grades[_id] = write_listing(i, listing)
 
 if st.button("Print Selected Grades", key="submit_button"):
     for _id, grade in selected_grades.items():
